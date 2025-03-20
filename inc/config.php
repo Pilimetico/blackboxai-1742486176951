417cb5a5ba25beccc617aa110a074c2ec55fd73d
Pilimetico <pilimetico01@gmail.com>
2025-03-20 10:56:22 -0500

    Add inc/config.php 1742486182915

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php
+// Configuración de la base de datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'raffle_db');
+
+// Configuraciones adicionales
+define('WHATSAPP_NUMBER', '+1234567890');
+define('BLOCK_OPTION', false);
+define('BLOCK_MINUTES', 0);
+?>
